dd7b405b89a941c766700a6819881c66a18cb0b8
Cristina-Colcha <criscolcha@gmail.com>
2024-11-10 09:49:22 -0500

    feat: :tada: New file MultiplicacionTablaController.java

---
+++ src/main/java/com/multiplicar/tabla_multiplicar/MultiplicacionTablaController.java
@@ -0,0 +1,22 @@
+package com.multiplicar.tabla_multiplicar;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class MultiplicacionTablaController {
+
+    @GetMapping("/table/{number}")
+    public String getMultiplicationTable(@PathVariable int number) {
+        StringBuilder table = new StringBuilder("<h1>Tabla de multiplicar del " + number + ":</h1><ul>");
+        for (int i = 1; i <= 10; i++) {
+            table.append("<li>").append(number).append(" x ").append(i).append(" = ").append(number * i).append("</li>");
+        }
+        table.append("</ul>");
+        return table.toString();
+    }
+}
