e45400f25aba415a3fe96d88c29613dffa481891
Cristina-Colcha <criscolcha@gmail.com>
2024-11-10 09:44:00 -0500

    feat: :tada: Inicio

---
+++ src/main/java/com/multiplicar/tabla_multiplicar/TablaMultiplicarApplication.java
@@ -0,0 +1,13 @@
+package com.multiplicar.tabla_multiplicar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TablaMultiplicarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TablaMultiplicarApplication.class, args);
+	}
+
+}
