7b548d170fedd320712a82d687637b37086619ca
Cristina-Colcha <criscolcha@gmail.com>
2024-11-12 22:44:30 -0500

    feat: :tada: vista nueva

--- src/main/java/com/multiplicar/tabla_multiplicar/MultiplicacionTablaController.java
+++ src/main/java/com/multiplicar/tabla_multiplicar/MultiplicacionTablaController.java
@@ -1,19 +1,29 @@
 package com.multiplicar.tabla_multiplicar;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
+@Controller
 public class MultiplicacionTablaController {
 
-    @GetMapping("/table/{number}")
-    public String getMultiplicationTable(@PathVariable int number) {
+    @GetMapping("/")
+    public String showForm(Model model) {
+        // Se inicializa con una tabla vacía
+        model.addAttribute("table", "");
+        return "index"; // Carga index.html
+    }
+
+    @PostMapping("/")
+    public String getMultiplicationTable(@RequestParam int number, Model model) {
         StringBuilder table = new StringBuilder("<h1>Tabla de multiplicar del " + number + ":</h1><ul>");
         for (int i = 1; i <= 12; i++) {
             table.append("<li>").append(number).append(" x ").append(i).append(" = ").append(number * i).append("</li>");
         }
         table.append("</ul>");
-        return table.toString();
+        model.addAttribute("table", table.toString()); // Pasa la tabla generada al modelo
+        return "index"; // Devuelve a la misma página
     }
 }
